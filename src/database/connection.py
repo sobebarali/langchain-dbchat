59cb9fec2d7c6c8145cbbd04562c1e1bded324ea
sobebarali <sobebar.ali17@gmail.com>
2024-11-08 16:33:18 +0530

    Add Chinook database connection setup
    Set up in-memory SQLite database connection for the Chinook database using SQLAlchemy with a static pool. This allows dynamic data retrieval and manipulation within the application without external database dependencies. Facilitates testing and development with the Chinook dataset by integrating SQL script fetching from a public URL and executing it within an in-memory database.

---
+++ src/database/connection.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+import requests
+from langchain_community.utilities.sql_database import SQLDatabase
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+
+
+def get_engine_for_chinook_db():
+    """Pull sql file, populate in-memory database, and create engine."""
+    url = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql"
+    response = requests.get(url)
+    sql_script = response.text
+
+    connection = sqlite3.connect(":memory:", check_same_thread=False)
+    connection.executescript(sql_script)
+    return create_engine(
+        "sqlite://",
+        creator=lambda: connection,
+        poolclass=StaticPool,
+        connect_args={"check_same_thread": False},
+    )
+
+
+engine = get_engine_for_chinook_db()
+
+db = SQLDatabase(engine)
